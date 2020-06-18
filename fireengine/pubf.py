add758202a9fe64c8602342b58d467cd14a4191b
Smiley <43641640+PSGSmiley@users.noreply.github.com>
2020-06-18 23:33:14 +0200

    Add files via upload

--- fireengine/pubf.py
+++ fireengine/pubf.py
@@ -1,17 +1,10 @@
 import paho.mqtt.client as mqtt
 import time
 import datetime
+import json
 
-task = "Bob"
-c1 = "32.51"
-c2 = "28.31"
-reason = "Dead"
-sendtask = (str(task)+" "+str(c1)+","+str(c2)+" "+reason)
-task1 = "Hans"
-c11 = "213.52"
-c21 = "53.234"
-reason1 = "Still_Alive"
-sendtask1 = (str(task1)+" "+str(c11)+","+str(c21)+" "+reason1)
+
+
 
 
 #Event, dass beim Verbindungsaufbau aufgerufen wird
@@ -20,30 +13,43 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe('/hshl/firefighters/f1')
     client.subscribe('/hshl/firefighters/f2')
     client.subscribe('/hshl/firefighters/f3')
-    print(sendtask)
+    data = {
+        "task": "Bob",
+        "location": [float(32.51), float(28.31)],
+        "reasons": "heart_attack",
+        "topic": "hshl/firefighters/f1"}
     topic = "/hshl/firefighters/f1"
-    client.publish(topic, sendtask)
-    print(sendtask1)
-    topic1 = "/hshl/firefighters/f2"
-    client.publish(topic1, sendtask1)
+    client.publish(topic, json.dumps(data))
+    data = {
+        "task": "Hans",
+        "location": [float(213.52), float(53.234)],
+        "reasons": "accident",
+        "topic": "hshl/firefighters/f2"}
+    topic = "/hshl/firefighters/f2"
+    client.publish(topic, json.dumps(data))
+    data = {
+        "task": "Bob",
+        "location": [float(32.51), float(28.31)],
+        "reasons": "heart_attack",
+        "topic": "hshl/firefighters/f1"}
+    topic = "/hshl/firefighters/f1"
+    client.publish(topic, json.dumps(data))
 
 def on_message(client, userdata, message):
     msg = str(message.payload.decode("utf-8")) #Nachricht Dekodieren
     currentDT = datetime.datetime.now() #Aktuelle Uhrzeit
     print(currentDT.strftime("%Y-%m-%d %H:%M:%S")+" Nachricht erhalten: "+str(msg))
-    split = msg.split(" ")
     i=0
     a = message.topic
     a = (a.split("/"))
     print(a)
-    if(split[1] == "True"):
-        print("True")#id and True or Flase
-    elif(a[3] == "f1"):
+    if(a[3] == "f1"):
         print("F1")
     elif(a[3] == "f2"):
         print("F2")
     else:
         print("Unknown topic")
+
 
 #Dont change anything from here!!
 BROKER_ADDRESS = "mr2mbqbl71a4vf.messaging.solace.cloud" #Adresse des MQTT Brokers
