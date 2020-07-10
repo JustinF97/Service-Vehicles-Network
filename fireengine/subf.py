951b0a5299620feb19f2ea3c4038f58ed99833d5
Smiley <43641640+PSGSmiley@users.noreply.github.com>
2020-07-10 11:51:53 +0200

    Add files via upload

--- fireengine/subf.py
+++ fireengine/subf.py
@@ -61,7 +61,8 @@ def make_police(avv):
             po.append("f"+str(i+1))
             po.append(str(randloc1)+","+str(randloc2))
             loc1 = [float(randloc1), float(randloc2)]
-            coor.append("["+str(randloc1)+", "+ str(randloc2)+"]")
+            test = [randloc1, randloc2]
+            coor.append(test)
             coor.append(loc1)
             coor.append("f"+str(i+1))
             topic="/hshl/firefighters/"
@@ -69,7 +70,7 @@ def make_police(avv):
             data = {
                 "time": currentDT.strftime("%Y-%m-%d %H:%M:%S"),
                 "driver_name": str(po[0+j]),
-                "location": ("["+str(randloc1)+", "+ str(randloc2)+"]"),
+                "location": test,
                 "isFree" : True,
                 "id": "f"+str(i+1),
                 "topic": topic}
@@ -150,7 +151,7 @@ def vehicle_returned(split, po):
                         global task
                         global av
                         global coor
-                        f = "["+b[0]+", "+b[1]+"]"
+                        f = [b[0], b[1]]
                         topic = ("/hshl/firefighters/"+str(tr[x]))
                         data = {
                             "time": currentDT.strftime("%Y-%m-%d %H:%M:%S"),
@@ -189,7 +190,7 @@ def check(split, task, po):
                 global coor
                 x = tr.index(str(split[0]))
                 if(str(tr[x+1]) == "True"):
-                    f = "["+b[0]+", "+b[1]+"]"
+                    f = [b[0], b[1]]
                     topic = ("/hshl/firefighters/"+str(tr[x]))
                     data = {
                         "time": currentDT.strftime("%Y-%m-%d %H:%M:%S"),
